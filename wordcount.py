786467bc4f14a4d6abef28e5ca18b09e56665c91
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:28:44 -0800

    added iteritems()

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,20 @@
-paragraph = open("twain.txt")
-word_count = {}
+def get_word_count(filename):
+
+    word_count = {}
+
+    with open(filename) as paragraph:
+        for line in paragraph:
+            line = line.rstrip()
+            words = line.split(" ")
+            for word in words:
+                word_count[word] = word_count.get(word, 0) + 1
+
+    return word_count
 
 
-for line in paragraph:
-    line = line.rstrip()
-    words = line.split(" ")
-    for word in words:
-        word_count[word] = word_count.get(word, 0) + 1
+for word, num in get_word_count('test.txt').items():
+    print "{} {}".format(word, num)
 
-print word_count
+for word, num in get_word_count('twain.txt').iteritems():
+    print "{} {}".format(word, num)
 
-
-# cohort = open('cohort_data.txt')
-#     for line in cohort:
-#         student = line.split('|')
-#         houses.add(student[2])
